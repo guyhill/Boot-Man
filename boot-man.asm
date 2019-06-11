23fa763b3936da64a53be734b6a298a845e45f13
Guido van den Heuvel <guyhill@xs4all.nl>
2019-06-11 13:54:10 +0200

    documenting the code

--- boot-man.asm
+++ boot-man.asm
@@ -1,67 +1,147 @@
-org 0x7c00
-bits 16
+; Boot-Man
+;
+; (c) 2019 Guido van den Heuvel
+;
+; Boot-Man is a Pac-Man clone that fits (snugly) inside the Master Boot Record of a USB stick.
+; A USB stick with Boot-Man on it boots into the game (hence the name). Unfortunately, however,
+; Boot-Man leaves no room in the MBR for a partition table, which means that a USB stick with Boot-Man
+; in its MBR cannot be used to store data. In fact, Windows does not recognize a USB stick with 
+; Boot-Man in its MBR as a valid storage medium.
+;
+; Controls of the game: you control Boot-Man using the WASD keys. No other user input is necessary. Some other
+; keys can also be used to control Boot-Man, this is a side effect of coding my own keyboard handler in 
+; just a few bytes. There simply wasn't room for checking the validity of every key press.
+;
+; The game starts automatically, and when Boot-Man dies, a new game starts automatically within a couple of seconds.
+;
+;
+; I've had to take a couple of liberties with the original Pac-Man game to fit Boot-Man inside the 510
+; bytes available in the MBR:
+;
+; * The ghosts start in the four corners of the maze, they do not emerge from a central cage like in the original
+;
+; * There's just a single level. If you finish the level, the game keeps running with an empty maze. While 
+;   it is rather difficult to finish the game (which is intentional, because of the single level), it is possible. 
+;
+; * Boot-Man only has 1 life. If Boot-Man dies, another game is started automatically (by re-reading the MBR
+;   from disk, there simply isn't enough room to re-initialize the game in any other way)
+;
+; * Power pills function differently from the original. When Boot-Man eats a power pill, all ghosts become
+;   ethereal (represented in game by just their eyes being visible) and cease to chase Boot-Man. While ethereal,
+;   Boot-Man can run through ghosts with no ill effects. While I would really like to include the "ghost eating"
+;   from the original, which I consider to be an iconic part of the game, this simply isn't possible in the little
+;   space available.
+;
+; * There's no score, and no fruit to get bonus points from.
+; 
+; * All ghosts, as well as Boot-Man itself, have the same, constant movement speed. In the original, the ghosts
+;   run at higher speeds than Pac-Man, while Pac-Man gets delayed slightly when eating and ghosts get delayed when moving
+;   through the tunnel connecting both sides of the maze. This leads to very interesting dynamics and strategies
+;   in the original that Boot-Man, by necessity, lacks.
+;
+;
+; Boot-Man runs in text mode. It uses some of the graphical characters found in IBM codepage 437 for its objects:
+;   - Boot-Man itself is represented by the smiley face (☻), which is character 0x02 in the IBM charset
+;   - The Ghosts are represented by the infinity symbol (∞), which is character 0xec. These represent
+;     a ghost's eyes, with the ghost's body being represented simply by putting the character on a 
+;     coloured background
+;   - The dots that represent Boot-Man's food are represented by the bullet character (•), 
+;     which is character 0xf9
+;   - The power pills with which Boot-Man gains extra powers are represented by the diamond (♦),
+;     which is character 0x04
+;   - The walls of the maze are represented by the full block character (█), which is character 0xdb
+;
+; Boot-Man runs off int 8, which is connected to the timer interrupt. It should therefore run at the same speed
+; on all PCs. It includes its own int 9 (keyboard) handler. The code is quite heavily optimized for size, so
+; code quality is questionable at best, and downright atrocious at worst.
+
+
+org 0x7c00                          ; The MBR is loaded at 0x0000:0x7c00 by the BIOS
+bits 16                             ; Boot-Man runs in Real Mode. I am assuming that the BIOS leaves the CPU is Real Mode.
+                                    ; This is true for the vast majority of PC systems. If your system's BIOS
+                                    ; switches to Protected Mode or Long Mode during the boot process, Boot-Man
+                                    ; won't run on your machine.
 
 start:
-    cli
+    cli                             ; Disable interrupts, as we are going to set up interrupt handlers and a stack
     xor ax, ax          
-    mov ds, ax
-    mov [bootdrive], dl ; save the current drive number, which has been stored in dl by the BIOS
+    mov ds, ax                      ; Set up a data segment that includes the Interrupt Vector Table and the Boot-Man code
+    mov [bootdrive], dl             ; save the current drive number, which has been stored in dl by the BIOS
     mov ss, ax          
-    mov sp, 0x28        ; Set up temporary stack within the interrupt vector table
-    push ax             ; This saves some bytes when setting up interrupt vectors
-    push word int9handler   
+    mov sp, 0x28                    ; Set up a temporary stack, within the interrupt vector table
+    push ax                         ; This saves some bytes when setting up interrupt vectors
+    push word int9handler           ; Set up my own int 9 (keyboard) handler
     push ax
-    push word int8handler
+    push word int8handler           ; Set up my own int 8 (timer interrupr) handler
     
-    mov sp, ax          ; Set up the real stack. This is safe to do as interrupts have been disabled
+    mov sp, ax                      ; Set up the real stack.
 
-    inc ax              ; Go to 40x25 character video mode
-    int 0x10            
+    inc ax                          ; int 0x10 / ah = 0: Switch video mode. Switch mode to 40x25 characters (al = 1). 
+    int 0x10                        ; In this mode, characters are approximately square, which means that horizontal 
+                                    ; and vertical movement speeds are almost the same.
 
-    mov cx, 0x2000      ; Remove cursor from screen
-    mov ah, 0x01
+    mov cx, 0x2000                  ; int 0x10 / ah = 1: Determine shape of hardware cursor. 
+    mov ah, 0x01                    ; With cx = 0x2000, this removes the hardware cursor from the screen altogether
     int 0x10
 
 
-    cld
+    cld                             ; Clear the direction flag. We use x86 string instructions a lot as they have one-byte codes
     mov ax, 0xb800
-    mov es, ax          ; es points to video RAM
+    mov es, ax                      ; Set up the es segment to point to video RAM
 
+
+;-----------------------------------------------------------------------------------------------------
+; buildmaze: builds the maze. The maze is stored in memory as a bit array, with 1 representing a wall
+;            and 0 representing a food dot. Since the maze is left-right symmetrical, only half of the
+;            maze is stored in memory. The positions of the power pills is hard-coded in the code.
+;            Adding the power pills to the bit array would have necessitated 2 bits for every 
+;            character, increasing its size drastically.
+;
+;            Both sides of the maze are drawn simultaneously. The left part is drawn left to right,
+;            while the right part is drawn right to left. For efficiency reasons, the entire maze 
+;            is built from the bottom up. Therefore, the maze is stored upside down in memory
+;-----------------------------------------------------------------------------------------------------
 buildmaze:
     mov di, 0x0788                  ; Lower left corner of maze in video ram
-    mov si, maze
-    mov dx, 0x05fa                  ; Address in video ram of lower left powerpill
+    mov si, maze                    ; The first byte of the bit array containing the maze
+    mov dx, 0x05fa                  ; Address in video ram of the lower left powerpill
 .maze_outerloop:
-    mov cx, 0x003c
-    lodsw
+    mov cx, 0x003c                  ; The distance between a character in the maze and its 
+                                    ; symmetric counterpart. Also functions as loop counter
+    lodsw                           ; Read 16 bits from the bit array, which represents one
+                                    ; 32 character-wide row from the maze
 .maze_innerloop:
-    shl ax, 1
+    shl ax, 1                       ; shift out a single bit to determine whether a wall or dot must be shown
     push ax
-    mov ax, 0x01db                  ; wall character (blue solid square)
-    jc .draw
-    mov ax, 0x0ff9                  ; food character (white dot)
-    cmp di, dx
-    jnz .draw
+    mov ax, 0x01db                  ; Assume it is a wall character (blue solid block)
+    jc .draw                        ; Draw the character if a 1 was shifted out
+    mov ax, 0x0ff9                  ; otherwise, assume a food character (white bullet)
+    cmp di, dx                      ; See if instead of food we need to draw a power pill
+    jnz .draw                       
     mov dh, 0x00                    ; Update powerpill address to draw remaining powerpills
-    mov al, 0x04                    ; powerpill character (white diamond)
+    mov al, 0x04                    ; powerpill character (white diamond - no need to set up colour once more)
 .draw:
-    stosw
+    stosw                           ; Store character + colour in video ram
     push di
-    add di, cx
-    stosw
+    add di, cx                      ; Go to its symmetric counterpart
+    stosw                           ; and store it as well
     pop di
     pop ax
-    sub cx, 4
-    jns .maze_innerloop
+    sub cx, 4                       ; Update the distance between the two sides of the maze
+    jns .maze_innerloop             ; As long as the distance between the two halves is positive, we continue
 
-    sub di, 0x70
-    jns .maze_outerloop
+    sub di, 0x70                    ; Go to the previous line on the screen in video RAM. 
+    jns .maze_outerloop             ; Keep going as long as this line is on screen.
 
-    sti
-.end:
-    mov al, 0x20
-    out 0x20, al
-    jmp .end
+
+    sti                             ; Initialization is complete, hence we can enable interrupts
+
+.end:                               ; Idle loop, as everything else is done in the interrupt handlers.
+                                    ; Unfortunately there's no room for a hlt here so the CPU keeps running 100%.
+    mov al, 0x20                    ; Continuously write "end of interrupt". This saves a few bytes in the 
+    out 0x20, al                    ; interrupt handlers themselves, as we have to do it only once, here.
+    jmp .end                        ; Overall, not a good way to implement an idle loop, its only saving grace 
+                                    ; being that it is so short.
 
 
 int8handler:
